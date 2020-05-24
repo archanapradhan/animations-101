fd33a528536bd1bb24b520e6e5a5d1a01b3dc5f9
Archana Pradhan <mail.archanapradhan@gmail.com>
2020-05-25 00:31:39 +1200

    first commit,simple animations on a basic app. Added Modal/Backdrop open, Modal/Backdrop close methods

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+class App extends Component {
+  state = {
+    isModalOpen: false,
+  };
+
+  closeModal = () => {
+    this.setState({ isModalOpen: false });
+  };
+
+  showModal = () => {
+    this.setState({ isModalOpen: true });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <Modal show={this.state.isModalOpen} closed={this.closeModal} />
+        <Backdrop show={this.state.isModalOpen} />
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Backdrop.css";
+
+const backdrop = (props) => {
+  const backdropStyles = [
+    "Backdrop",
+    props.show ? "BackdropOpen" : "BackdropClose",
+  ];
+  return <div className={backdropStyles.join(" ")}></div>;
+};
+export default backdrop;
