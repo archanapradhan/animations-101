1f9b543ceca1f7b8eb95a0cc91aca7960cd2a143
Archana Pradhan <mail.archanapradhan@gmail.com>
2020-06-07 00:31:09 +1200

    added transition properties in, timeout and showing the varius states of transition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -7,6 +8,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     isModalOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -21,8 +23,53 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.isModalOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.isModalOpen} />
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({
+              showBlock: !prevState.showBlock,
+            }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+
+        <Transition in={this.state.showBlock} timeout={1000}>
+          {/* By wrapping an element in Transition componenet 
+          we can control the display of these elements.
+          Most common use of Transition is to animate the mounting and unmouting 
+          of a compoenent, also can be used for transition  */}
+          {/* following code will display the current state of Transition Entering->Entered->Exiting->Exited */}
+          {(state) => <p>{state}</p>}
+          {/* <div
+            style={{
+              height: 100,
+              width: 100,
+              margin: "25px auto",
+
+              backgroundColor: "red",
+            }}
+          ></div> */}
+        </Transition>
+        {/* {this.state.showBlock ? (
+          <div
+            style={{
+              height: 100,
+              width: 100,
+              margin: "25px auto",
+
+              backgroundColor: "red",
+            }}
+          ></div>
+        ) : null} */}
+
+        {this.state.isModalOpen ? (
+          <Modal show={this.state.isModalOpen} closed={this.closeModal} />
+        ) : null}
+        {this.state.isModalOpen ? (
+          <Backdrop show={this.state.isModalOpen} />
+        ) : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
